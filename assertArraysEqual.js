b74d23634293d8df036169e720a6c4175bdec0be
jonathanddickie <jonathanddickie@gmail.com>
2021-07-20 22:49:06 +0000

    initial commit: created assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const eqArrays = function(array1, array2) {
+  let equal = true;
+  if (array1.length !== array2.length) {
+    equal = false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      equal = false;
+    }
+  }
+  if (equal === true) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} === ${array2}`);
+  }
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]);
+eqArrays([1, 2, 3], [3, 2, 1]);
