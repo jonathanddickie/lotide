685aa221f630c84fd35d9ec2350d3d5fa8591867
jonathan dickie <jonathanddickie@gmail.com>
2021-07-26 19:20:40 +0000

    added code to assertEqualTest.js to test ../tail.js

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,5 +1,8 @@
 const assertEqual = require('../assertEqual');
 const head = require('../head');
+const tail = require('../tail');
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(tail([5,6,7]), 7);
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Labs");
